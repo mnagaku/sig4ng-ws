cbce8e2a86a44ed0c3b9eb1366ca4e0f1f3f9032
mnagaku <nagaku@atomitech.jp>
2016-06-05 13:17:17 +0900

    BGM付けた

---
+++ Assets/script/bgm.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgm : MonoBehaviour {
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.clip = Resources.Load("335571__magntron__gamemusic") as AudioClip;
+		audioSource.loop = true;
+		audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
