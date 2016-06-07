4e4f210a6a1e486bbc1c091c4848fde57820289f
mnagaku <nagaku@atomitech.jp>
2016-06-07 14:03:58 +0900

    カメラ動くようになったけど、まだ瞬間移動

--- Assets/script/gate.cs
+++ Assets/script/gate.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class gate : MonoBehaviour {
 
-	private Camera camera = Camera.main;
 	private bool mflag = false;
 	private bool fflag = true;
 	private float targetX, targetY, targetZ;
@@ -11,23 +10,19 @@ public class gate : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		targetX = (float)(this.transform.position.x - 3.5);
-		targetY = (float)(this.transform.position.x + 6);
-		targetZ = (float)(this.transform.position.x - 13);
+		targetY = (float)(this.transform.position.y + 6);
+		targetZ = (float)(this.transform.position.z - 11);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(mflag) {
-			Vector3 newPosition = camera.transform.position;
-	        newPosition.x = targetX;
-	        newPosition.y = targetY;
-	        newPosition.z = targetZ;
-	        camera.transform.position = newPosition;
+	        Camera.main.transform.position = new Vector3(targetX, targetY, targetZ);
 			mflag = false;
 		}
 	}
 
-	void OnTriggerEnter(Collision other) {
+	void OnTriggerEnter(Collider other) {
 		if(fflag) {
 			mflag = true;
 			fflag = false;
