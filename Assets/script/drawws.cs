1367d491efc32202d2abd6eb98b2be43e495ecaa
mnagaku <nagaku@atomitech.jp>
2016-06-09 00:12:41 +0900

    お絵描きWS連携実装

---
+++ Assets/script/drawws.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drawws : MonoBehaviour {
+
+	public int charNo = 7;
+
+	private GameObject armr, arml;
+	private bool flag = false;
+
+	// Use this for initialization
+	IEnumerator Start () {
+		string bodyURL
+			= string.Format("http://kgr-lab.ddo.jp/drawws/b{0:0000}.png", charNo);
+		string armURL
+			= string.Format("http://kgr-lab.ddo.jp/drawws/a{0:0000}.png", charNo);
+
+		WWW bodyWWW = new WWW(bodyURL);
+		WWW armWWW = new WWW(armURL);
+		yield return bodyWWW;
+		yield return armWWW;
+
+		GameObject body = transform.FindChild ("body").gameObject;
+		Renderer bodyRenderer = body.GetComponent<Renderer>();
+		bodyRenderer.material.mainTexture = bodyWWW.texture;
+
+		armr = transform.FindChild ("armr").gameObject;
+		Renderer armrRenderer = armr.GetComponent<Renderer>();
+		armrRenderer.material.mainTexture = armWWW.texture;
+
+		arml = transform.FindChild ("arml").gameObject;
+		Renderer armlRenderer = arml.GetComponent<Renderer>();
+		armlRenderer.material.mainTexture = armWWW.texture;
+
+		flag = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(flag) {
+			armr.transform.Rotate(0, 5, 0);
+			arml.transform.Rotate(0, -5, 0);
+		}
+	}
+}
