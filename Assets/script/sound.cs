10993fc5a211b1fac5fc2bc008bc4130b7856666
mnagaku <nagaku@atomitech.jp>
2016-06-17 23:29:06 +0900

    10面まで作った。衝突音を差し替えやすいように修正。

--- Assets/script/sound.cs
+++ Assets/script/sound.cs
@@ -4,16 +4,21 @@ using System.Collections.Generic;
 
 public class sound : MonoBehaviour {
 
+	public AudioClip ac;
+
 	private bool sflag = true;
 	private float x, z;
 	private AudioSource audioSource;
 
 	// Use this for initialization
 	void Start () {
+		if(!ac) {
+			ac = Resources.Load("235968__tommccann__explosion-01") as AudioClip;
+		}
 		x = transform.localPosition.x;
 		z = transform.localPosition.z;
 		audioSource = gameObject.AddComponent<AudioSource>();
-		audioSource.clip = Resources.Load("235968__tommccann__explosion-01") as AudioClip;
+		audioSource.clip = ac;
 		audioSource.time = 1;
 	}
 	
