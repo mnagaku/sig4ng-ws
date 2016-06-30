d926965ee7b5c6087a88dd11e1f5e1ae3a4c42ce
Makoto Goto <mgomao@gmail.com>
2016-06-30 22:02:03 +0900

    Clear Stage 追加

---
+++ Assets/script/clear_area.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clear_area : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+
+		Application.LoadLevel("clear");
+	}
+}
---
+++ Assets/script/clear_click.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clear_click : MonoBehaviour {
+
+	float timer = 0.0f;
+	int count = 0;
+
+	UnityEngine.UI.Text uiText;
+
+	// Use this for initialization
+	void Start () {
+		uiText = this.gameObject.GetComponent<UnityEngine.UI.Text>() as UnityEngine.UI.Text;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timer += Time.deltaTime;
+//		if (timer >= 1.0f)
+		{
+			timer = 0.0f;
+
+			var col = uiText.color;
+			count++;
+			col = ((count & 0x04) == 0) ? Color.white : Color.black;
+			uiText.color = col;
+		}
+	}
+
+
+	public void ClickNext(string next)
+	{
+		Application.LoadLevel(next);
+	}
+}
