6857d9c903a6d8d709c1b8ab33999784956ec7c3
mnagaku <mnagaku@gmail.com>
2017-08-30 16:57:23 +0900

    お絵描きWSのscriptを修正、Unityを2017.1.0f3に更新

--- Assets/script/drawws.cs
+++ Assets/script/drawws.cs
@@ -1,45 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class drawws : MonoBehaviour {
-
-	public int charNo = 7;
+public class drawws : MonoBehaviour
+{
+	public int charNo = 1;
+	public string siteName = "https://drawws.kgr-lab.com";
 
 	private GameObject armr, arml;
 	private bool flag = false;
 
 	// Use this for initialization
-	IEnumerator Start () {
-		string bodyURL
-			= string.Format("http://kgr-lab.ddo.jp/drawws/b{0:0000}.png", charNo);
-		string armURL
-			= string.Format("http://kgr-lab.ddo.jp/drawws/a{0:0000}.png", charNo);
+	IEnumerator Start ()
+	{
+		GameObject body = transform.Find ("body").gameObject;
+		Renderer bodyRenderer = body.GetComponent<Renderer> ();
 
-		WWW bodyWWW = new WWW(bodyURL);
-		WWW armWWW = new WWW(armURL);
-		yield return bodyWWW;
-		yield return armWWW;
+		armr = transform.Find ("armr").gameObject;
+		Renderer armrRenderer = armr.GetComponent<Renderer> ();
 
-		GameObject body = transform.FindChild ("body").gameObject;
-		Renderer bodyRenderer = body.GetComponent<Renderer>();
-		bodyRenderer.material.mainTexture = bodyWWW.texture;
+		arml = transform.Find ("arml").gameObject;
+		Renderer armlRenderer = arml.GetComponent<Renderer> ();
 
-		armr = transform.FindChild ("armr").gameObject;
-		Renderer armrRenderer = armr.GetComponent<Renderer>();
-		armrRenderer.material.mainTexture = armWWW.texture;
+		string bodyFile = string.Format ("/b{0:0000}", charNo);
+		string armFile = string.Format ("/a{0:0000}", charNo);
 
-		arml = transform.FindChild ("arml").gameObject;
-		Renderer armlRenderer = arml.GetComponent<Renderer>();
-		armlRenderer.material.mainTexture = armWWW.texture;
+		Texture2D tbody;
+		Texture2D tarm;
+		tbody = Resources.Load<Texture2D> ("Texture" + bodyFile);
+		tarm = Resources.Load<Texture2D> ("Texture" + armFile);
+
+		if (tbody == null || tarm == null) {
+			WWW bodyWWW = new WWW (siteName + bodyFile + ".png");
+			WWW armWWW = new WWW (siteName + armFile + ".png");
+			yield return bodyWWW;
+			yield return armWWW;
+			bodyRenderer.material.mainTexture = bodyWWW.texture;
+			armrRenderer.material.mainTexture = armWWW.texture;
+			armlRenderer.material.mainTexture = armWWW.texture;
+		} else {
+			bodyRenderer.material.mainTexture = tbody;
+			armrRenderer.material.mainTexture = tarm;
+			armlRenderer.material.mainTexture = tarm;
+		}
 
 		flag = true;
 	}
 
 	// Update is called once per frame
-	void Update () {
-		if(flag) {
-			armr.transform.Rotate(0, 5, 0);
-			arml.transform.Rotate(0, -5, 0);
+	void Update ()
+	{
+		if (flag) {
+			armr.transform.Rotate (0, 5, 0);
+			arml.transform.Rotate (0, -5, 0);
 		}
 	}
 }
