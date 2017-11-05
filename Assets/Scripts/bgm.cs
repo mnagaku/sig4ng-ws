1d07fa746231c6a8f782fe5a637bb77f0e08405a
mnagaku <mnagaku@gmail.com>
2017-11-05 16:45:04 +0900

    ステージ毎にマテリアルを分けた

---
+++ Assets/Scripts/bgm.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgm : MonoBehaviour {
+
+	public AudioClip ac;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.clip = ac;
+		audioSource.loop = true;
+		audioSource.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/Scripts/sound.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class sound : MonoBehaviour {
+
+	public AudioClip ac;
+
+	private bool sflag = true;
+	private float x, z;
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		if(!ac) {
+			ac = Resources.Load("SE/bomb") as AudioClip;
+		}
+		x = transform.localPosition.x;
+		z = transform.localPosition.z;
+		audioSource = gameObject.AddComponent<AudioSource>();
+		audioSource.clip = ac;
+		audioSource.time = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(sflag && 
+			(System.Math.Abs(transform.localPosition.x - x) > 0.1 ||
+			System.Math.Abs(transform.localPosition.z - z) > 0.1)) {
+			audioSource.Play();
+			sflag = false;
+		}
+	}
+}
