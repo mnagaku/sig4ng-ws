c3bac2277917909d4438df0a133b6d4270be3396
mnagaku <mnagaku@gmail.com>
2025-01-27 16:59:17 +0900

    Update .gitignore

---
+++ Assets/DevLocker/VersionControl/WiseGit/Editor/ContextMenus/SnailGitContextMenus.cs
@@ -0,0 +1,281 @@
+// MIT License Copyright(c) 2022 Filip Slavov, https://github.com/NibbleByte/UnityWiseGit
+
+using DevLocker.VersionControl.WiseGit.Shell;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+namespace DevLocker.VersionControl.WiseGit.ContextMenus.Implementation
+{
+	// SnailGit: https://langui.net/snailgit/
+	// Use the "/Applications/SnailGit.app/Contents/Resources/snailgit.sh" executable as much as possible.
+	// usage: /Applications/SnailGitLite.app/Contents/Resources/snailgit.sh <subcommand> [args]
+	internal class SnailGitContextMenus : GitContextMenusBase
+	{
+		private const string ClientLightCommand = "/Applications/SnailGitLite.app/Contents/Resources/snailgit.sh";
+		private const string ClientPremiumCommand = "/Applications/SnailGit.app/Contents/Resources/snailgit.sh";
+
+		private const string ClientLightProtocol = "snailgitfree";
+		private const string ClientPremiumProtocol = "snailgit";
+
+		protected override string FileArgumentsSeparator => " ";
+		protected override bool FileArgumentsSurroundQuotes => true;
+
+		private ShellUtils.ShellResult ExecuteCommand(string command, string workingFolder, bool waitForOutput)
+		{
+			return ExecuteCommand(command, string.Empty, workingFolder, waitForOutput);
+		}
+
+		private ShellUtils.ShellResult ExecuteCommand(string command, string fileArgument, string workingFolder, bool waitForOutput)
+		{
+			return ShellUtils.ExecuteCommand(new ShellUtils.ShellArgs() {
+				Command = File.Exists(ClientPremiumCommand) ? ClientPremiumCommand : ClientLightCommand,
+				Args = string.IsNullOrEmpty(fileArgument) ? command : $"{command} {fileArgument}",
+				WorkingDirectory = workingFolder,
+				WaitForOutput = waitForOutput,
+				WaitTimeout = -1,
+			});
+		}
+
+		private void ExecuteProtocol(string action, string workingFolder)
+        {
+			if (!workingFolder.StartsWith('/')) {
+				workingFolder = Path.Combine(WiseGitIntegration.ProjectRootNative, workingFolder);
+            }
+
+			// The snailgit.sh currently doesn't accept somne actions, but with some reverse engineering, managed to make it work like this.
+			// open "snailgitfree://git-merge/SomeFolderHere/UnityProject/Assets"
+			string url = $"{GetClientProtocol()}://{action}{System.Uri.EscapeUriString(workingFolder)}";
+			Application.OpenURL(url);
+		}
+
+		private void ExecuteProtocol(string action, string workingFolder, string path)
+		{
+			if (!workingFolder.StartsWith('/')) {
+				workingFolder = Path.Combine(WiseGitIntegration.ProjectRootNative, workingFolder);
+			}
+			if (!path.StartsWith('/')) {
+				path = Path.Combine(WiseGitIntegration.ProjectRootNative, path);
+			}
+
+			// The snailgit.sh currently doesn't accept somne actions, but with some reverse engineering, managed to make it work like this.
+			// open "snailgitfree://git-merge/SomeFolderHere/UnityProject/Assets"
+			string url = $"{GetClientProtocol()}://{action}{System.Uri.EscapeUriString(workingFolder)}?{System.Uri.EscapeUriString(path)}";
+			Application.OpenURL(url);
+		}
+
+		private string GetClientProtocol() => File.Exists(ClientPremiumCommand) ? ClientPremiumProtocol : ClientLightProtocol;
+
+		public override void CheckChanges(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (assetPaths.Count() == 1 && assetPaths.All(File.Exists)) {
+				DiffChanges(assetPaths.First(), wait);
+				return;
+            }
+
+			// Doesn't support cleanup command (doesn't seem to have such a window?)
+			UnityEditor.EditorUtility.DisplayDialog("Not supported", "Sorry, check changes functionality is currently not supported by SnailGit.", "Sad");
+		}
+
+		public override void DiffChanges(string assetPath, bool wait = false)
+		{
+			var result = ExecuteCommand("diff", Path.Combine(WiseGitIntegration.ProjectRootNative, assetPath), WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+
+		public override void Pull(bool wait = false)
+		{
+			var result = ExecuteCommand("pull", WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+
+		public override void Merge(bool wait = false)
+		{
+			ExecuteProtocol("git-merge", WiseGitIntegration.ProjectRootNative);
+		}
+
+		public override void Fetch(bool wait = false)
+		{
+			var result = ExecuteCommand("fetch", WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+
+		public override void Push(bool wait = false)
+		{
+			var result = ExecuteCommand("push", WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+
+		public override void SubmodulesUpdate(bool wait = false)
+		{
+			UnityEditor.EditorUtility.DisplayDialog("Not Supported", "SnailGit does not support git submodules yet.", "OK");
+		}
+
+		public override void Commit(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			// If trying to commit directory that was just added, show the parent, so the directory meta is visible. Exclude root "Assets" and "Packages" folder.
+			var fixedPaths = assetPaths.Select(path =>
+					(path.Contains('/') && Directory.Exists(path) && WiseGitIntegration.GetStatus(path).Status == VCFileStatus.Added)
+					? Path.GetDirectoryName(path)
+					: path
+					);
+
+			var result = ExecuteCommand("commit", GetWorkingPath(assetPaths), wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+
+		public override void Add(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			foreach (var path in assetPaths) {
+				if (!WiseGitIntegration.CheckAndAddParentFolderIfNeeded(path, true))
+					return;
+			}
+
+			var metas = assetPaths
+				.Select(path => path + ".meta")
+				;
+
+			var pathsArg = AssetPathsToContextPaths(includeMeta ? assetPaths.Concat(metas) : assetPaths, false);
+
+			var result = ExecuteCommand("add", pathsArg, WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			} else {
+				// No window is shown, add happens instantly.
+				GitStatusesDatabase.Instance.InvalidateDatabase();
+			}
+		}
+
+		public override void Revert(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			var metas = assetPaths
+				.Select(path => path + ".meta")
+				;
+
+			var pathsArg = AssetPathsToContextPaths(includeMeta ? assetPaths.Concat(metas) : assetPaths, false);
+
+			var result = ExecuteCommand("revert", pathsArg, WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+
+		public override void ResolveAll(bool wait = false)
+		{
+			ExecuteProtocol("git-resolve", WiseGitIntegration.ProjectRootNative);
+		}
+
+		public override void Resolve(string assetPath, bool wait = false)
+		{
+			// Doesn't support resolve command (doesn't seem to have such a window?)
+			UnityEditor.EditorUtility.DisplayDialog("Not supported", "Sorry, resolve functionality is currently not supported by SnailGit.", "Sad");
+		}
+
+
+		public override void GetLocks(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			if (assetPaths.Any(Directory.Exists)) {
+				UnityEditor.EditorUtility.DisplayDialog("Cannot Lock Directories", "Directory locking is not supported. Please select specific files instead.", "Ok");
+				return;
+			}
+
+			// Doesn't support locks (doesn't seem to have such a window?)
+			using (var reporter = new WiseGitIntegration.ResultConsoleReporter(true, WiseGitIntegration.Silent, "SnailGitContextMenus Operations:")) {
+				var result = WiseGitIntegration.LockFiles(assetPaths, false, WiseGitIntegration.ONLINE_COMMAND_TIMEOUT, reporter);
+				if (result != LockOperationResult.Success) {
+					Debug.LogError($"Git Error: {result}");
+				} else {
+					GitStatusesDatabase.Instance.InvalidateDatabase();
+				}
+			}
+		}
+
+		public override void ReleaseLocks(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			if (assetPaths.Any(Directory.Exists)) {
+				UnityEditor.EditorUtility.DisplayDialog("Cannot Lock Directories", "Directory locking is not supported. Please select specific files instead.", "Ok");
+				return;
+			}
+
+			// Doesn't support locks (doesn't seem to have such a window?)
+			using (var reporter = new WiseGitIntegration.ResultConsoleReporter(true, WiseGitIntegration.Silent, "SnailGitContextMenus Operations:")) {
+				var result = WiseGitIntegration.UnlockFiles(assetPaths, false, WiseGitIntegration.ONLINE_COMMAND_TIMEOUT, reporter);
+				if (result != LockOperationResult.Success) {
+					Debug.LogError($"Git Error: {result}");
+				} else {
+					GitStatusesDatabase.Instance.InvalidateDatabase();
+				}
+			}
+		}
+
+		public override void ShowLog(string assetPath, bool wait = false)
+		{
+			if (string.IsNullOrEmpty(assetPath))
+				return;
+
+			var workingFolder = Directory.Exists(assetPath) ? assetPath : Path.GetDirectoryName(assetPath);
+
+			ExecuteProtocol("git-log", workingFolder, assetPath);
+		}
+
+		public override void Blame(string assetPath, bool wait = false)
+		{
+			if (string.IsNullOrEmpty(assetPath))
+				return;
+
+			// Support only one file.
+
+			var result = ExecuteCommand("blame", assetPath, WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+
+		public override void Cleanup(bool wait = false)
+		{
+			// Doesn't support cleanup command (doesn't seem to have such a window?)
+			UnityEditor.EditorUtility.DisplayDialog("Not supported", "Sorry, clean up functionality is currently not supported by SnailGit.", "Sad");
+		}
+
+		public override void RepoBrowser(string path, string remoteBranch, bool wait = false)
+		{
+			var workingFolder = Directory.Exists(path) ? path : Path.GetDirectoryName(path);
+
+			ExecuteProtocol("repo-browser", workingFolder);
+		}
+
+		public override void Switch(bool wait = false)
+		{
+			var result = ExecuteCommand("checkout", WiseGitIntegration.ProjectRootNative, wait);
+			if (result.HasErrors) {
+				Debug.LogError($"Git Error: {result.Error}");
+			}
+		}
+	}
+}
---
+++ Assets/DevLocker/VersionControl/WiseGit/Editor/Utils/EditorPersistentSingleton.cs
@@ -0,0 +1,75 @@
+// MIT License Copyright(c) 2022 Filip Slavov, https://github.com/NibbleByte/UnityWiseGit
+
+using System.Linq;
+using UnityEngine;
+
+namespace DevLocker.VersionControl.WiseGit.Utils
+{
+	public interface IEditorPersistentSingleton
+	{
+		void Initialize(bool freshlyCreated);
+	}
+
+	/// <summary>
+	/// Base class for singletons that have to survive assembly reloads.
+	/// It uses hidden ScriptableObject.
+	/// </summary>
+	public abstract class EditorPersistentSingleton<SingletonType> : ScriptableObject, IEditorPersistentSingleton
+		where SingletonType : ScriptableObject, IEditorPersistentSingleton
+	{
+
+		private static bool m_SingletonInitialized;
+		private static SingletonType m_Instance;
+
+		public static SingletonType Instance {
+			get {
+				bool freshlyCreated = false;
+
+				if (m_Instance == null) {
+					ScriptableObject.CreateInstance<SingletonType>(); // No need to assign it - the constructor will do it.
+					m_Instance.name = m_Instance.GetType().Name;	// nameof(SingletonType) returns "SingletonType".
+
+					// Setting this flag will tell Unity NOT to destroy this object on assembly reload (as no scene references this object).
+					// We're essentially leaking this object. But we can still get a reference to it after reload,
+					// when Unity recreates the existing scriptable objects (this will call the object constructor during deserialization).
+					// More info on this: https://blogs.unity3d.com/2012/10/25/unity-serialization/
+					m_Instance.hideFlags = HideFlags.HideAndDontSave;
+
+					freshlyCreated = true;
+				}
+
+				// Data is already deserialized by Unity onto the scriptable object.
+				// Even though OnEnable is not yet called, data is there after assembly reload.
+				// It is deserialized even before static constructors [InitializeOnLoad] are called. I tested it! :D
+
+				// The idea here is to save some time on assembly reload from deserializing json as the reload is already slow enough for big projects.
+
+				if (!m_SingletonInitialized || freshlyCreated) {	// Freshly created also, just in case?
+					m_SingletonInitialized = true;
+					m_Instance.Initialize(freshlyCreated);
+				}
+
+				return m_Instance;
+			}
+
+
+		}
+
+
+		protected EditorPersistentSingleton()
+		{
+			// Constructor will get called on calling Instance OR during deserialization on assembly reload.
+			// If this is assembly reload, fields will not be deserialized yet - initialize on demand, not here.
+			// This method was inspared by the Unity ScriptableSingleton:
+			// https://docs.unity3d.com/2020.1/Documentation/ScriptReference/ScriptableSingleton_1.html
+			if (m_Instance == null) {
+				m_Instance = this as SingletonType;
+
+			} else {
+				Debug.LogError($"{GetType().Name} singleton found another instance! Abort!", this);
+			}
+		}
+
+		public abstract void Initialize(bool freshlyCreated);
+	}
+}
